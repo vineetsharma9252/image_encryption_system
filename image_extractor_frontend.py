178af14554f4a7867c5c75fb76a39daeba4dfc00
vineet <Mastergenos228@gmail.com>
2025-02-03 19:43:44 +0530

    "Second commit"

--- image_extractor_frontend.py
+++ image_extractor_frontend.py
@@ -5,7 +5,6 @@ import matplotlib.image as mpimg
 from PIL import Image
 st.set_page_config(initial_sidebar_state='collapsed')
 page = st.sidebar.selectbox("Select a page", ["Home", "About", "Contact"])
-# Title
 
 from Components.home import home_page
 from Components.about import About_page
